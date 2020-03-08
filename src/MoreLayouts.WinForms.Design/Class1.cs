204a56119f6ca9d36d5aca5500c9000507e903e8
Oleg Kucherenko <kukumber@gmail.com>
2020-03-08 14:39:44 +0300

    Add MoreLayouts.WinForms.Design project

---
+++ src/MoreLayouts.WinForms.Design/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreLayouts.WinForms.Design
+{
+    public class Class1
+    {
+    }
+}
